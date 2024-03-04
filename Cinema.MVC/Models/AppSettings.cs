c34edeabe2d24663e353d72f5d0037b243d084cd
regen <regen@MUYEN>
2024-03-04 16:54:45 +0300

    Add project files.

---
+++ Cinema.MVC/Models/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Cinema.MVC.Models
+{
+    public class AppSettings
+    {
+        public string ApplicationName { get; set; }
+        public string ApiBaseUrl { get; set; }
+    }
+}
+ 
---
+++ Cinema.MVC/Models/MovieViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinema.MVC.Models
+{
+    public class MovieViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
